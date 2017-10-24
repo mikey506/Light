7849ccdd71d380281d3459082f5cd66e7a4ebbbd
Michael Landry <michaellandry3@hotmail.com>
2017-10-24 16:38:58 -0300

    edited quote file location

--- modules/Quotes.pm
+++ modules/Quotes.pm
@@ -5,25 +5,24 @@ package Quotes;
 
 my $bot  = Shadow::Core;
 use File::RandomLine;
-my $rl = File::RandomLine->new( "~/quotes" );
+my $rl = File::RandomLine->new( "./quotes" );
 
 sub loader {
-	$bot->add_handler('chancmd quote', 'doQuote');
+  $bot->add_handler('chancmd quote', 'doQuote');
   my ($nick, $host, $text) = @_;
-	$bot->say($nick, "Help for \x02QUOTE\x02:");
-	$bot->say($nick, "-------------------------");
-	$bot->say($nick, "\x02quote\x02 print a random, Quote");
-	$bot->say($nick, "\x02SYNTAX\x02: .quote");
+  $bot->say($nick, "Help for \x02QUOTE\x02:");
+  $bot->say($nick, "-------------------------");
+  $bot->say($nick, "\x02quote\x02 print a random, Quote");
+  $bot->say($nick, "\x02SYNTAX\x02: .quote");
 }
 
 sub doQuote {
-	my ($nick, $host, $chan, $text) = @_;
+  my ($nick, $host, $chan, $text) = @_;
   $line = $rl->next();
-	$bot->say($chan, "Your random Quote $nick : $line" );
+  $bot->say($chan, "Your random Quote $nick : $line" );
 }
 
 sub unloader {
-	$bot->del_handler('chancmd quote', 'doQuote');
+  $bot->del_handler('chancmd quote', 'doQuote');
 }
-
 1;
