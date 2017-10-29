f81db916904fbb23239e06ba8c38aad535b90128
Michael Landry <michaellandry3@hotmail.com>
2017-10-29 03:18:24 -0300

    Added IRC::Bot::Seen and join capturing

--- modules/iSeen.pm
+++ modules/iSeen.pm
@@ -12,20 +12,29 @@ package iSeen;
 # Version Info:
 # - v0.1 - In early development
 #
-
+use IRC::Bot::Seen;
 my $bot = Shadow::Core;
+my $seen = IRC::Bot::Seen->new();
+
 sub loader {
   $bot->add_handler('chancmd seen', 'iseen_id');
+  $bot->add_handler('cmdchan join', 'iseen_join');
+}
+
+sub iseen_join {
+  $seen->log_seen( $nick, "Joining $chan" );
+  print "logging $nick joining $chan";
 }
 
 sub iseen_id {
   my ($nick, $host, $chan, $text) = @_;
   my ($user, $channel, @reason);
   my $bhost = $Shadow::Core::sc{lc($chan)}{users}{$nick}{host};
-  $bot->log("[iSeen Module] (UserInfo) User: $nick Host: $host \n"); 
-  $bot->say($nick, "I seen that you seen");
+  $bot->say($chan, "$nick $text last seen $seen->get_seen( $text ); 
+  $seen->log_seen( $nick, "Joining $chan" );
 }
 sub unloader {
   $bot->del_handler('chancmd seen', 'iseen_id');
+  $bot->del_handler('cmdchan join', 'iseen_join');
 }
 1;
