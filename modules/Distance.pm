7964cac699521bf5894a49b5b01823afc8540a9f
Michael Landry <michaellandry3@hotmail.com>
2017-11-03 09:17:16 -0300

    Fixed a few bugs and a bad loop

--- modules/Distance.pm
+++ modules/Distance.pm
@@ -1,10 +1,11 @@
 package Distance;
 
+ use Data::Dumper;
  use GIS::Distance;
  my $bot = Shadow::Core;
  my $gis = GIS::Distance->new();
-
-sub loader {
+ 
+ sub loader {
    $bot->add_handler('chancmd dist', 'doDistance');
 }
 
@@ -12,9 +13,24 @@ sub doDistance {
   print "dodistance detected\n";
   my ($nick, $host, $chan, $text) = @_;
   my @fields = split / /, $text;
-  print ("fields array: (0) ". $fields[0] ." (1) ".$fields[1]." (2)  ".$fields[2]." (3)  ".$fields[3]." (4)  ".$fields[4]."\n");
-  my $distance = $gis->distance( $fields[0],$fields[1] => $fields[2],$fields[3] );
-  $bot->say($chan, "Distance: $distance km for: ( $fields[0] $fields[1] => $fields[2] $fields[3] )");
+  print ("fields array (c2c): (0) ". $fields[0] ." (1) ".$fields[1]." (2)  ".$fields[2]." (3)  ".$fields[3]." (4)  ".$fields[4]."\n");
+  open my $fh, '<', './etc/geocities.db' or die "Cannot open file: $!\n";
+  my @file = <$fh>;
+  foreach $line (@file) {
+    my @linestr = split / /, $line;
+    if ($linestr[0] =~ /$fields[0]/ || $linestr[0] =~ /^$fields[0]/) {
+      $chomp;
+      $c2c1 = $linestr[1,2];
+      $c2c1 =~ s/[\$#@~!&*()\[\];,:?^`\\\/]+//g;
+      $bot->say($chan, "Found following entry in database for ( $linestr[0] ): $linestr[1] $linestr[2] ");
+    }
+    if ($linestr[0] =~ /$fields[1]/ || $linestr =~ /^$fields[1]/) {
+      $chomp;
+      $c2c2 = $linestr[1,2];
+      $c2c2 =~ s/[\$#@~!&*()\[\];,:?^`\\\/]+//g;
+      $bot->say($chan, "Found following entry in database for ( $linestr[0] ): $linestr[1] $linestr[2] ");
+    }
+  }
 }
 
 sub unloader {
