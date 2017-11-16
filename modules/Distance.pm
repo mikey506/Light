0aeede143a00ecbb5ca9f847b071d7836afbbc42
Michael Landry <michaellandry3@hotmail.com>
2017-11-16 14:03:55 -0400

    More functional

--- modules/Distance.pm
+++ modules/Distance.pm
@@ -12,25 +12,28 @@ package Distance;
 sub doDistance {
   my ($nick, $host, $chan, $text) = @_;
   my @fields = split / /, $text;
-  open my $fh, '<', './etc/geocities.db' or die "Cannot open file: $!\n";
+  open my $fh, '<', './etc/geocities.db' or die print "Cannot open file: $!\n";
   my @file = <$fh>;
   foreach $line (@file) {
     my @linestr = split / /, $line;
     if ($linestr[0] =~ /$fields[0]/ || $linestr[0] =~ /^$fields[0]/) {
       $chomp;
-      $c2c1 = $linestr[1,2];
-      $c2c1 =~ s/[\$#@~!&*()\[\];,:?^`\\\/]+//g;
+      $c1_1 = $linestr[1];
+      $c1_2 = $linestr[2];
       $bot->say($chan, "Found following entry in database for ( $linestr[0] ): $linestr[1] $linestr[2] ");
     }
     if ($linestr[0] =~ /$fields[1]/ || $linestr =~ /^$fields[1]/) {
       $chomp;
-      $c2c2 = $linestr[1,2];
-      $c2c2 =~ s/[\$#@~!&*()\[\];,:?^`\\\/]+//g;
+      $c2_1 = $linestr[1];
+      $c2_2 = $linestr[2];
       $bot->say($chan, "Found following entry in database for ( $linestr[0] ): $linestr[1] $linestr[2] ");
     }
   }
-  print "(Distance Mod) $nick ." requested distance from ". $fields[0] ." to ". $fields[1] ." . Total Kilometers: ". $distance ."\n";
-  $bot->say($chan, "$nick, your requested distance info from $fields[0] to $fields[1] - Total Kilometers:  $distance");
+  $distance = $gis->distance( $c1_1 , $c1_2 => $c2_1 , $c2_2 );
+  $distance = sprintf("%.3f", $distance);
+  print "Debug: ". $c1_1 ." ". $c2_1 ." ". $c1_2 ." ". $c2_2; 
+  print "(Distance Mod) ". $nick ." requested distance from ". $fields[0] ." to ". $fields[1] ." Total Kilometers: ". $distance ." km\n";
+  $bot->say($chan, "$nick, your requested distance info from $fields[0] to $fields[1] - Total Distance: $distance km");
 }
 sub unloader {
   $bot->del_handler('chancmd dist', 'doDistance');
